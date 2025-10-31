30316bde7a43cef400ae9d2f571089761569eb30
NovofyDev <82439877+NovofyDev@users.noreply.github.com>
2025-10-31 03:04:36 +0000

    Updated settings.py and urls.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -44,7 +44,11 @@ DATABASES = {
     }
 }
 
-ALLOWED_HOSTS = ['*']
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f"{CODESPACE_NAME}-8000.app.github.dev")
 
 # Application definition
 
